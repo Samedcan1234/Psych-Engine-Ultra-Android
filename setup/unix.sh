45b62eb45d4f1c00ffbab9d09706e4de3968e346
SametGkTe <sametgkte@gmail.com>
2026-03-11 17:18:11 +0300

    dneme124

--- setup/unix.sh
+++ setup/unix.sh
@@ -8,7 +8,7 @@ mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
-haxelib install lime 8.1.3 --quiet
+haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib install openfl 9.4.1 --quiet
 haxelib git flixel https://github.com/kittycathy233/flixel --quiet
 haxelib install flixel-addons 3.3.2 --quiet
@@ -21,5 +21,4 @@ haxelib install hxdiscord_rpc --quiet --skip-dependencies
 haxelib install hxvlc 2.0.1 --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
-haxelib git extension-androidtools https://github.com/jigsaw-4277821/extension-androidtools --quiet
 echo Finished!
